af4b2a8b6d287392330acc798e0a0e96aa299871
David Roel Gómez <david.roel@businessmind.es>
2024-07-31 18:16:42 +0200

    First commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,45 +1,36 @@
-import type { CartItem, Guitar } from "../types"
+import { Dispatch, useMemo } from 'react'
+import type { CartItem } from '../types'
+import { CartActions } from '../reducers/cartReducer'
 
 type HeaderProps = {
     cart: CartItem[]
-    removeFromCart: (id: Guitar['id'] ) => void
-    decreaseQuantity: (id: Guitar['id'] ) => void
-    increaseQuantity: (id: Guitar['id'] ) => void
-    clearCart: () => void
-    isEmpty: boolean
-    cartTotal: number
+    dispatch: Dispatch<CartActions>
 }
 
-export default function Header({
-        cart, 
-        removeFromCart, 
-        decreaseQuantity, 
-        increaseQuantity, 
-        clearCart,
-        isEmpty, 
-        cartTotal
-    } : HeaderProps ) {
+export default function Header({cart, dispatch} : HeaderProps) {
+    // State Derivado
+    const isEmpty = useMemo(() => cart.length === 0, [cart])
+    const cartTotal = useMemo(() => cart.reduce((total, item) => total + (item.quantity * item.price), 0), [cart])
+
     return (
-        <header className="py-5 header">
-            <div className="container-xl">
-                <div className="row justify-content-center justify-content-md-between">
-                    <div className="col-8 col-md-3">
-                        <a href="index.html">
-                            <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
+        <header className='py-5 header'>
+            <div className='container-xl'>
+                <div className='row justify-content-center justify-content-md-between'>
+                    <div className='col-8 col-md-3'>
+                        <a href='index.html'>
+                            <img className='img-fluid' src='/img/logo.svg' alt='imagen logo' />
                         </a>
                     </div>
-                    <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-                        <div 
-                            className="carrito"
-                        >
-                            <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
+                    <nav className='col-md-6 a mt-5 d-flex align-items-start justify-content-end'>
+                        <div className='carrito'>
+                            <img className='img-fluid' src='/img/carrito.png' alt='imagen carrito' />
 
-                            <div id="carrito" className="bg-white p-3">
+                            <div id='carrito' className='bg-white p-3'>
                                 {isEmpty ? (
-                                    <p className="text-center">El carrito esta vacio</p>
+                                    <p className='text-center'>El carrito esta vacio</p>
                                 ) : (
                                 <>
-                                    <table className="w-100 table">
+                                    <table className='w-100 table'>
                                         <thead>
                                             <tr>
                                                 <th>Imagen</th>
@@ -50,41 +41,41 @@ export default function Header({
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            {cart.map( guitar => (
+                                            {cart.map(guitar => (
                                                 <tr key={guitar.id}>
                                                     <td>
                                                         <img 
-                                                            className="img-fluid" 
+                                                            className='img-fluid' 
                                                             src={`/img/${guitar.image}.jpg`}
-                                                            alt="imagen guitarra" 
+                                                            alt='imagen guitarra' 
                                                         />
                                                     </td>
                                                     <td>{guitar.name}</td>
-                                                    <td className="fw-bold">
+                                                    <td className='fw-bold'>
                                                         ${guitar.price}
                                                     </td>
-                                                    <td className="flex align-items-start gap-4">
+                                                    <td className='flex align-items-start gap-4'>
                                                         <button
-                                                            type="button"
-                                                            className="btn btn-dark"
-                                                            onClick={() => decreaseQuantity(guitar.id)}
+                                                            type='button'
+                                                            className='btn btn-dark'
+                                                            onClick={() => dispatch({type: 'decrease-quantity', payload: {id: guitar.id}})}
                                                         >
                                                             -
                                                         </button>
                                                             {guitar.quantity}
                                                         <button
-                                                            type="button"
-                                                            className="btn btn-dark"
-                                                            onClick={() => increaseQuantity(guitar.id)}
+                                                            type='button'
+                                                            className='btn btn-dark'
+                                                            onClick={() => dispatch({type: 'increase-quantity', payload: {id: guitar.id}})}
                                                         >
                                                             +
                                                         </button>
                                                     </td>
                                                     <td>
                                                         <button
-                                                            className="btn btn-danger"
-                                                            type="button"
-                                                            onClick={() => removeFromCart(guitar.id)}
+                                                            className='btn btn-danger'
+                                                            type='button'
+                                                            onClick={() => dispatch({type: 'remove-from-cart', payload: {id: guitar.id}})}
                                                         >
                                                             X
                                                         </button>
@@ -94,13 +85,13 @@ export default function Header({
                                         </tbody>
                                     </table>
 
-                                    <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal}</span></p>
+                                    <p className='text-end'>Total pagar: <span className='fw-bold'>${cartTotal}</span></p>
                                 </>
                                 )}
 
                                 <button 
-                                    className="btn btn-dark w-100 mt-3 p-2"
-                                    onClick={clearCart}
+                                    className='btn btn-dark w-100 mt-3 p-2'
+                                    onClick={() => dispatch({type: 'clear-cart'})}
                                 >Vaciar Carrito</button>
                             </div>
                         </div>
